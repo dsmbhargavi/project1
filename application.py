a79cee5dbbe4a691a06d345be81e07418e5dc2bc
Sree Mallika Bhargavi Dhulipati <dsmbhargavi@msitprogram.net>
2020-09-26 19:01:30 +0530

    login problem

--- application.py
+++ application.py
@@ -1,22 +1,18 @@
 import os
 from sqlalchemy import or_
-
-from flask import Flask,render_template,request,session
+import time
+from flask import Flask,render_template,request,session,redirect
+from register import *
 # from flask import Flask, session
 from flask_session import Session
 from sqlalchemy import create_engine
 # from sqlalchemy.orm import  sessionmaker
 
 app = Flask(__name__)
-a=create_engine("postgres://ijnjabxiugykvh:46457019d5117fecc06ba03fa7a39080893a27b4980e382f0bf60f193fd95587@ec2-34-234-185-150.compute-1.amazonaws.com:5432/d6nvgp7e7umvqn")
+# a=create_engine("postgres://ijnjabxiugykvh:46457019d5117fecc06ba03fa7a39080893a27b4980e382f0bf60f193fd95587@ec2-34-234-185150.compute1.amazonaws.com:5432d6nvgp7e7umvqn", echo=True)
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
-
-# # Configure session to use filesystems
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
-# Session(app)
 
 # Configure session to use filesystem
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
@@ -36,54 +32,56 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 def index():
     return render_template("index.html")
 
-@app.route("/registration",methods=["POST"])
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/user")
+def user():
+    return render_template("user.html")
+
+@app.route("/registration",methods=["POST","GET"])
 def registration():
-    Name=request.form.get("Name")
-    UserName = request.form.get("UserName")
-    Email=request.form.get("Email")
-    Password=request.form.get("Password")
-    # print(Name,UserName,Email,Password)
-    # return Name+" ,"+Email
-     # check if user existed or not
+    if request.method =="POST":
+        Name=request.form.get("Name")
+        UserName = request.form.get("UserName")
+        Email=request.form.get("Email")
+        Password=request.form.get("Password")
+        # print(Name,UserName,Email,Password)
+        #   return Name+" ,"+Email
         userData = User.query.filter_by(email=Email).first()
 
         if userData is not None:
             return render_template("login.html", message="email already exists, Please login.")
         else:
-            user = User(name=Name,username=UserName,email=email, password=Password,time_registered=time.ctime(time.time()))
+            user = User(name=Name,username=UserName,email=Email, password=Password,time_registered=time.ctime(time.time()))
+            print("user creation done")
+
+            # print(db.session.queryall)
+            # print(user.queryall)
 
             # check if all details were given for registration.
             try:
                 db.session.add(user)
                 db.session.commit()
-                return name+" ,"+email+","+time_registered
-                 # return render_template("user.html",  username=UserName, message="Successfully Registered")
-
-             except:
-                 return render_template("registration.html", message="Fill all the details!")
-    # return "<h1>Please Register</h1>"
+                return render_template("user.html",  username=UserName, message="Successfully Registered")
+            except:
+                return render_template("index.html", message="Fill details!")
+        return render_template("index.html", message="Register")
 
 
-    @app.route("/login",methods=["POST"])
-    def login():
-            username = request.form.get('username')
-            usr_pas = request.form.get('password')
-
-            # check if user existed or not
-            userData = User.query.filter_by(username=username).first()
-
-            # if user is present, validate username and password
-            if userData is not None:
-                if userData.username == username and userData.password == usr_pas:
-                    session[username] = username
-                    return username;
-                    # return redirect(url_for('userHome', user=username))
-                # user verification failed
-                else:
-                    return render_template("registration.html", message="username/password is incorrect!!")
-            # if user doesn't exists.
+@app.route("/ldetails",methods=["POST","GET"])
+def ldetails():
+    if request.method =="POST":
+        username = request.form.get('username')
+        userpass = request.form.get('password')
+        userData = User.query.filter_by(username=username).first()
+        if userData is not None:
+            if userData.username == username and userData.password == userpass:
+                return render_template("user.html",message="Sucess")
             else:
-                return render_template("registration.html", message="Account doesn't exists, Please register!")
-        # # if try to access directly
-        # else:
-        #     return "<h1>Please login/register instead.</h1>"
+                return render_template("login.html", message="details incorrect!!")
+        else:
+            return render_template("index.html", message="Account doesn't exists, Go to Registration")
+    else:
+        return render_template("login.html")
